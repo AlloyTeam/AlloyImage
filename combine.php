e7f1b54f7d6548fc8bd228df4ec785c3999a0232
honghu91 <honghu91@hotmail.com>
2013-01-24 09:52:11 +0800

    fix

--- combine.php
+++ combine.php
@@ -1,12 +1,12 @@
 <?php
 $files = scandir("js/module/");
-$contents = file_get_contents("js/psLib.js");
+$contents = file_get_contents("js/alloyimage.base.js");
 foreach($files as $file){
     if(preg_match("/[^\.]+\.js$/",$file)){
         $contents .= file_get_contents("js/module/".$file);
     }
 }
-$wfile = fopen("js/combined/psLib_com.js","w+");
+$wfile = fopen("js/combined/alloyimage.js","w+");
 if(fwrite($wfile,$contents)){
     echo "OK";
 }
