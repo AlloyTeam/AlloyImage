01278132bdc68e95f723cc631a46c89c6b6dee03
honghu91 <honghu91@hotmail.com>
2013-02-03 23:45:20 +0800

    规范化

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 (function(){
-    var MsgBox = function(title,msg,inhtml){
+    var MsgBox = function(title, msg, inhtml){
+
+      //初始化参数
       this.width = "500px";
       this.height = "300px";
       this.title = title;
       this.msg = msg;
       this.i = 0;
       this.inhtml = inhtml;
+
       this.init = function(){
+
         this.i ++;
+
         var html = "<div class='commonmsgbox'><div class='title'>";
         html += this.title;
         html += "</div><div class='msgclose'>关闭</div><div class='commonmsg'>";
@@ -15,46 +20,66 @@
         html += "</div><div class='content'>";  
         html += this.inhtml;
         html += "</div></div>";
+
         if(this.i == 1) $(".wrapper").append(html);
         else $(".commonmsgbox").replaceWith(html);
+
         $(".commonmsgbox").css({width:this.width,height:this.height,display:"none"});
+
         var msgtop = (document.documentElement.clientHeight - parseInt($(".commonmsgbox").css("height")))/2;
         var msgleft = (document.documentElement.clientWidth > 1003) ? (document.documentElement.clientWidth - parseInt($(".commonmsgbox").css("width"))) / 2 : (1003 - parseInt($(".commonmsgbox").css("width"))) / 2;
+
         $(".commonmsgbox").css({"top":msgtop + "px","left":msgleft + "px"});
         $(".commonmsgbox .title").css("width",$(".commonmsgbox").css("width"));
+
         var _this = this;
+
         $(".commonmsgbox .msgclose").click(function(){
             _this.hide();
         });
-      }
+      };
+
       this.show = function(){
         $(".commonmsgbox").slideDown("fast");
       };
+
       this.hide = function(){
         this.inhtml = "";
         $(".commonmsgbox").html("");
         $(".commonmsgbox").slideUp("fast");
       };
+
       this.close = this.hide;
-    }
+    };
+
     var msg = new MsgBox("AlloyPhoto","","");
 
+    //曲线
     var Curve = {
         dotX: [],
         dotY: [],
         canvas: null,
         f: null,
+
+        //初始化
         init: function(wrapper){
             this.dorsyMath = AlloyImage.dorsyMath();
+
             var canvas = document.createElement("canvas");
             canvas.width = "400";
             canvas.height = "200";
             wrapper.append(canvas);
 
             this.canvas = canvas;
+
+            //绑定事件
             this.eventAtt();
         },
+
+        //画曲线
         draw: function(){
+
+            //获得langrange插值函数
             var f = this.dorsyMath.lagrange(this.dotX, this.dotY);
             var ctx = this.canvas.getContext("2d");
 
@@ -62,7 +87,7 @@
             var height = this.canvas.height;
             ctx.clearRect(0, 0, width, height);
 
-            for(var i = 0;i <this.dotX.length;i ++){
+            for(var i = 0; i <this.dotX.length; i ++){
                 var dx = this.dotX[i] / 255 * width;
                 var dy = (1 - this.dotY[i] / 255) * height;
                 ctx.beginPath();
@@ -70,64 +95,84 @@
                 ctx.closePath();
                 ctx.fill();
             }
+
             ctx.beginPath();
 
-            for(var i = 0;i < 256;i ++){
+            for(var i = 0; i < 256; i ++){
                 var x = i / 255 * width;
                 var y = height - f(i) / 255 * height;
+
                 y = y > height ? height : y;
                 y = y < 0 ? 0 : y;
+
                 ctx.lineTo(x,y);
             }
+
             ctx.stroke();
             ctx.closePath();
             
             this.f = f;
-            var func = this.func ? this.func : function(){};
+
+            var func = this.func || function(){};
+
             func(this.dotX, this.dotY);
         },
+
         setPoint: function(dotX, dotY){
             this.dotX = dotX;
             this.dotY = dotY;
             this.draw();
         },
+
         setObserver: function(func){
             this.func = func;
         },
+
         eventAtt: function(){
             var _this = this;
-            var lineOnFlag = 0,relate = null;//标记在曲线上
+
+            //标记在曲线上
+            var lineOnFlag = 0,relate = null;
+
             this.canvas.onmousemove = function(e){
-                var relate2 = null
-                var dx = e.offsetX ? e.offsetX : e.pageX;
-                var dy = e.offsetY ? e.offsetY : e.pageY;
+                var relate2 = null;
+                var dx = e.offsetX || e.pageX;
+                var dy = e.offsetY || e.pageY;
                 var x = dx / _this.canvas.width * 255;
                 var y = (1 - dy / _this.canvas.height) * 255;
                 var delta = _this.f(x) - y;
+
                 if(Math.abs(delta) < 9){
-                    for(var i = 0;i < _this.dotX.length;i ++){
+                    for(var i = 0; i < _this.dotX.length; i ++){
                         var delta = x - _this.dotX[i];
+
                         this.style.cursor = "pointer";
+
                         if(Math.abs(delta) < 9){
                             relate2 = true;
                             break;
                         }
                     }
+
                 }else{
                     this.style.cursor = "auto";
                 }
             };
+
             this.canvas.onmousedown = function(e){
-                var dx = e.offsetX ? e.offsetX : e.pageX;
-                var dy = e.offsetY ? e.offsetY : e.pageY;
+
+                var dx = e.offsetX || e.pageX;
+                var dy = e.offsetY || e.pageY;
                 var x = dx / _this.canvas.width * 255;
                 var y = (1 - dy / _this.canvas.height) * 255;
                 var delta = _this.f(x) - y;
+
                 if(Math.abs(delta) < 9){
                     lineOnFlag = 1;
 
-                    for(var i = 0;i < _this.dotX.length;i ++){
+                    for(var i = 0; i < _this.dotX.length; i ++){
                         var delta = x - _this.dotX[i];
+
                         if(Math.abs(delta) < 9){
                             relate = i;
                             break;
@@ -135,11 +180,13 @@
                     }
                 }
             };
+
             this.canvas.onmouseup = function(e){
                 if(lineOnFlag){
                     lineOnFlag = 0;
-                    var dx = e.offsetX ? e.offsetX : e.pageX;
-                    var dy = e.offsetY ? e.offsetY : e.pageY;
+
+                    var dx = e.offsetX || e.pageX;
+                    var dy = e.offsetY || e.pageY;
                     var x = dx / _this.canvas.width * 255;
                     var y = (1 - dy / _this.canvas.height) * 255;   
 
@@ -150,6 +197,7 @@
                         _this.dotX.push(x);
                         _this.dotY.push(y);
                     }
+
                     _this.draw();
                     
                     relate = null;
@@ -158,69 +206,97 @@
         }
     };
 
-    var Bar = {//对滑动bar的事件处理对象
+    //对滑动bar的事件处理对象
+    var Bar = {
         observer: [],
+
         notify: function(target){
             var value = parseInt(target.parent().find(".dMsg").text());
-            for(var i = 0;i < this.observer.length;i ++){
+
+            for(var i = 0; i < this.observer.length; i ++){
+
                if(target.parent().attr("id") == this.observer[i][0]){
                     this.observer[i][1](value);
                }
+
             }
         },
-        addObserver: function(targetId,func){
-            this.observer.push([targetId,func]);
+
+        addObserver: function(targetId, func){
+            this.observer.push([targetId, func]);
         }
     };
 
     var COM_MODEL = [
-    "正常","颜色减淡","变暗","变亮","正片叠底","滤色","叠加","强光","差值","排除","点光","颜色加深","线性加深","线性减淡","柔光","亮光","线性光","实色混合"
+        "正常", "颜色减淡", "变暗", "变亮", "正片叠底", "滤色", "叠加", "强光", "差值", "排除", "点光", "颜色加深", "线性加深", "线性减淡", "柔光", "亮光", "线性光", "实色混合"
     ];
 
     var COM_HTML_MODEL = "<select class='commodel'>";
-    for(var i = 0;i < COM_MODEL.length;i ++){
+
+    for(var i = 0; i < COM_MODEL.length; i ++){
         COM_HTML_MODEL += "<option value='" + COM_MODEL[i] + "'>" + COM_MODEL[i] + "</option>";
     }
+
     COM_HTML_MODEL += "</select>";
 
-    var layerCount = 0;//图层的命名计数
+    //图层的命名计数
+    var layerCount = 0;
     var Main = {
         layers: [],
-        ps: null,//主画布
-        layers:[],
-        currLayer:[],
-        openFile: function(fileUrl){//打开文件
+
+        //主画布
+        ps: null,
+
+        layers: [],
+
+        currLayer: [],
+
+        //打开文件
+        openFile: function(fileUrl){
 
             var reader = new FileReader();
             var _this = this;
+
             reader.readAsDataURL(fileUrl);
+
             reader.onload = function(){
                 msg.close();
+
                 var img = new Image();
                 img.src = this.result;
                 img.onload = function(){
                     _this.addImage(img);
                 };
+
             };
         
         },
         addImage: function(img){
+
             var psObj = AlloyImage(img);
+
             if(!(this.ps)){
                 this.ps = AlloyImage(parseInt(img.width),parseInt(img.height),"rgba(255,255,255,0)");
+
                 $(".left").css({width:img.width,height:img.height});
                 $(".openFile").html("画布区");
             }
 
             this.layers.push(psObj);
-            this.ps.addLayer(psObj);//添加一个图层
-            this.currLayer = [this.layers.length - 1];//设置当前图层
 
-            this.addLayer();//向面板添加一个图层
+            //添加一个图层
+            this.ps.addLayer(psObj);
+
+            //设置当前图层
+            this.currLayer = [this.layers.length - 1];
+            
+            //向面板添加一个图层
+            this.addLayer();
             this.draw();
         },
 
-        recieveImg:function(){//监听 Q+ rpc传来的文件做处理
+        //监听 Q+ rpc传来的文件做处理
+        recieveImg:function(){
             var _this = this;
             /*
            RpcMgr.recive( 'fdeee', function( data ){
@@ -248,14 +324,17 @@
             */
         },
 
-        addLayer: function(){//向图层面板添加一个图层
+        //向图层面板添加一个图层
+        addLayer: function(){
            var html = "<div class='lItem'><span class='layerName'>图层" + (++ layerCount) + "</span> 混合模式" + COM_HTML_MODEL + "</div>"; 
            $(".layer").prepend(html);
+
            this.showCurrLayer();
         },
-        
-        swap: function(a,b){//交换两个图层的位置  在layers数组中的位置交换，
-            this.ps.swap(a,b);
+
+        //交换两个图层的位置  在layers数组中的位置交换，
+        swap: function(a, b){
+            this.ps.swap(a, b);
 
             var temp = this.layers[a];
             this.layers[a] = this.layers[b];
@@ -267,6 +346,7 @@
             var htmlA= A.html();
             var selectA = A.find("select").val();
             var selectB = B.find("select").val();
+
             A.html(B.html());
             A.find("select option[value='" + selectB + "']").attr("selected","selected");
             B.html(htmlA);
@@ -282,12 +362,14 @@
 
             var _this = this;
 
-            $("#upFile").change(function(e){//上传文件处理
+            //上传文件处理
+            $("#upFile").change(function(e){
                 msg.title = "读取文件";
                 msg.msg = "正在读取文件...";
                 msg.inhtml = "<img src='style/image/03.gif' />";
                 msg.init();
                 msg.show();
+
                 _this.openFile(e.target.files[0]);
             });
 
@@ -314,28 +396,39 @@
                 }
 
                 _this.layers.push(psObj);
-                _this.ps.addLayer(psObj);//添加一个图层
-                _this.currLayer = [_this.layers.length - 1];//设置当前图层
 
-                _this.addLayer();//向面板添加一个图层
+                //添加一个图层
+                _this.ps.addLayer(psObj);
+
+                //设置当前图层
+                _this.currLayer = [_this.layers.length - 1];
+
+                //向面板添加一个图层
+                _this.addLayer();
+
                 _this.draw();
 
             });
 
             $("#modify").click(function(){
+
                 $(".pItem").hide("fast");
                 $("#upFile").hide();
                 $(".modifyItem").css("display","block");
                 $(".back").show();
+
             });
 
             $("#lj").click(function(){
                 $(".pItem").each(function(i){
+
                     if(i == 3) $(this).hide("fast",function(){
                         $(".ljItem").css("display","block");
                     });
                     else $(this).hide();
+
                 });
+
                 $("#upFile").hide();
                 $(".back").show();
             });
@@ -357,9 +450,11 @@
 
             $(".ljItem").click(function(){
                 var text = $(this).text();
+
                 for(var i = 0;i < _this.currLayer.length;i ++){
                     _this.layers[_this.currLayer[i]].act(text);
                 }
+
                 _this.draw();
             });
 
@@ -370,6 +465,7 @@
                 $(this).hide();
             });
             $("#modi_b").click(function(){
+
                 msg.title = "亮度/对比度调节";
                 msg.msg = "请滑动调节";
                 msg.inhtml = "亮　度:<div id='dBar1' class='dBar' rangeMin='-50' rangeMax='50'><a draggable='false' href='#'></a><div class='dMsg'>0</div></div><br />";
@@ -377,6 +473,7 @@
                 msg.inhtml += "<div class='dView'><button id='excute'>确定</button><button id='cancel'>取消</button></div>";
                 msg.init();
                 msg.show();
+
                 Bar.addObserver("dBar1",function(value){
                     var value2 = parseInt($("#dBar2 .dMsg").text());
                     for(var i = 0;i < _this.currLayer.length;i ++){
@@ -384,6 +481,7 @@
                     }
                     _this.draw();
                 });
+
                 Bar.addObserver("dBar2",function(value){
                     var value1 = parseInt($("#dBar1 .dMsg").text());
                     for(var i = 0;i < _this.currLayer.length;i ++){
@@ -392,7 +490,9 @@
                     _this.draw();
                 });
         });
+
             $("#modi_HSI").click(function(){
+
                 msg.title = "色相/饱和度调节";
                 msg.msg = "请滑动调节";
                 msg.inhtml = "色　相:<div id='dBar1' class='dBar' rangeMin='-180' rangeMax='180'><a draggable='false' href='#'></a><div class='dMsg'>0</div></div><br />";
@@ -411,6 +511,7 @@
                     }
                     _this.draw();
                 });
+
                 Bar.addObserver("dBar2",function(value){
                     var value1 = parseInt($("#dBar1 .dMsg").text());
                     var value3 = parseInt($("#dBar3 .dMsg").text());
@@ -420,6 +521,7 @@
                     }
                     _this.draw();
                 });
+
                 Bar.addObserver("dBar3",function(value){
                     var value1 = parseInt($("#dBar1 .dMsg").text());
                     var value2 = parseInt($("#dBar2 .dMsg").text());
@@ -430,6 +532,7 @@
                     _this.draw();
                 });
             });
+
         $("#curve").live("click", function(){
            msg.title = "曲线";
            msg.msg = "请点击或调节节点";
@@ -446,20 +549,25 @@
                 }
                 _this.draw();
            });
+
         });
 
         $("#excute").live("click",function(){
+
             for(var i = 0;i < _this.currLayer.length;i ++){
                 _this.layers[_this.currLayer[i]].excute();
             }
+
             _this.draw();
             msg.hide();
         });
 
         $("#cancel").live("click",function(){
+
             for(var i = 0;i < _this.currLayer.length;i ++){
                 _this.layers[_this.currLayer[i]].cancel();
             }
+
             _this.draw();
             msg.hide();
         });
@@ -468,22 +576,27 @@
             for(var i = 0;i < _this.currLayer.length;i ++){
                 _this.layers[_this.currLayer[i]].cancel();
             }
+
             _this.draw();
             msg.hide();
         });
 
-        var flagM1 = 0,flagD1 = 0;//滑动bar标记
+        var flagM1 = 0, flagD1 = 0;//滑动bar标记
         var orginOffsetX = 0;
-        var clientX = 0,offsetX = 0,dTarget;
+        var clientX = 0, offsetX = 0, dTarget;
+
         $(".dBar a").live("mousedown",function(e){
             flagM1 = 1;
             clientX = e.clientX;
             offsetX = parseInt($(this).css("left"));
             dTarget = $(this);
         });
+
         $(document).bind("mousemove",function(e){
             if(flagM1){
-                flagD1 = 1;//拖拽开始
+                
+                //拖拽开始
+                flagD1 = 1;
                 var dx = e.clientX - clientX; 
                 var currLeft = offsetX + dx;
                 var circleWidth = parseInt(dTarget.css("width")) / 2;
@@ -497,6 +610,7 @@
                 var rangeMax = parseFloat(dTarget.parent().attr("rangeMax")) || 0;
                 var percent = (currLeft + circleWidth) / parentWidth;
                 var nowRange = (rangeMin + (rangeMax - rangeMin) * percent).toFixed(0);
+
                 dTarget.parent().find(".dMsg").text(nowRange);
             }
         });
@@ -525,6 +639,7 @@
 
         var flagK = 0,flagM = 0,flagD = 0;//flagK 标记key alt flagM标记 Mouse flagD标记拖拽事件
         var dx = [],dy = [];
+
         $(document).keydown(function(e){
             if(e.keyCode == 17){
                 flagK = 1;
@@ -663,12 +778,17 @@
         },
 
         draw: function(isFast){
-                this.ps.show(".painting",isFast);//显示主画布
-                this.ps.drawRect();//重绘直方图
+                //显示主画布
+                this.ps.show(".painting",isFast);
+
+                //重绘直方图
+                this.ps.drawRect();
         },
 
-        showCurrLayer: function(){//显示当前操作或选中的图层
+        //显示当前操作或选中的图层
+        showCurrLayer: function(){
             $(".lItem").removeClass("blueStyle");
+
             for(var i = 0;i < this.currLayer.length;i ++){
                $(".lItem:eq(" + (this.layers.length - 1 - this.currLayer[i]) + ")").addClass("blueStyle");
             }
@@ -676,7 +796,5 @@
     };
 
     Main.init();
-
- 
 
 })();
