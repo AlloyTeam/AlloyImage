0782f07d0c9e37294b4dbe7a4c72b1e34969587e
honghu91 <honghu91@hotmail.com>
2013-02-21 16:14:43 +0800

    add e13

--- js/module/easy.js
+++ js/module/easy.js
@@ -75,6 +75,14 @@
                         return this.add(
                             m, "叠加"
                         );
+                    },
+
+                    //木雕的效果
+                    e13: function(){
+                        var layerClone = this.clone().act("马赛克").act("查找边缘").act("浮雕效果");
+                        return this.add(
+                            layerClone, "线性光"
+                        );
                     }
                 };
 
