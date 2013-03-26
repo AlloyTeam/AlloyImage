ae9a8e585c10cf3c766898b2814eec3175d3242a
honghu91 <honghu91@hotmail.com>
2013-03-26 17:29:01 +0800

    add e14

--- js/main2.js
+++ js/main2.js
@@ -224,7 +224,8 @@
                 "灰白" : "e10",
                 "灰色" : "e11",
                 "暖秋" : "e12",
-                "木雕" : "e13"
+                "木雕" : "e13",
+                "粗糙" : "e14"
             };
             var effectModel = '<li class="e_item"><div class="imgWrapper"><img src="style/image/demo/{pic}.png" alt="" />{effect}</div></li>';
             var html = '<li class="e_item"><div class="imgWrapper"><img src="style/image/demo/e1.jpg" alt="" />原图</div></li>';
